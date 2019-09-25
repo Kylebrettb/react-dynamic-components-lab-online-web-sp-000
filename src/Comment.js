c4dcec695e23dc25757771fd563a9e4dbfc7ca7c
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 19:58:34 +0000

    Automatically backed up by Learn

---
+++ src/Comment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+  
+  render() {
+    return (
+      <div className="comment">
+        {/* your conditional code here! */}
+        {this.props.commentText}
+      </div>
+    )
+  }
+  
+}
